be597ba11739f57754e03ceb6c846649638febe5
Lerskk <faustoleo02@gmail.com>
2022-12-02 08:10:46 -0300

    Add custom colors and path to src

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-gray': '#121212',
+        'cool-gray': {
+          50: '#f5f7fa',
+          100: '#e4e7eb',
+          200: '#cbd2d9',
+          300: '#9aa5b1',
+          400: '#7b8794',
+          500: '#616e7c',
+          600: '#52606d',
+          700: '#3e4c59',
+          800: '#323f4b',
+          900: '#1f2933',
+        },
+      },
+    },
   },
   plugins: [],
 };
