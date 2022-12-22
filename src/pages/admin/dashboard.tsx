9fab010da6039dae7fb65c2ea6eaf541fe3d4522
Kevin <challengerenlol@gmail.com>
2022-12-22 18:51:27 -0300

    sidebar done for desktop

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -1,10 +1,17 @@
 import Sidebar from '../../components/sidebar';
+import { type NextPage } from 'next';
 
-export default function Dashobard() {
+const Dashboard: NextPage = () => {
   return (
-    <div>
+    <div className='flex h-screen w-full items-center bg-cool-gray-900'>
+      <link
+        rel='stylesheet'
+        href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200'
+      />
       <Sidebar />
       dashboard
     </div>
   );
-}
+};
+
+export default Dashboard;
