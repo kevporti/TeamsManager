96a577da7820828e2caf201f72480ebcc1c92c8e
Kevin <challengerenlol@gmail.com>
2023-01-03 19:45:53 -0300

    Getting  data from inputs with 'react-hook-form' finished. Validation from frontend using 'zod' finished.

--- src/pages/admin/new-team-leader.tsx
+++ src/pages/admin/new-team-leader.tsx
@@ -1,9 +1,33 @@
 import { useState } from 'react';
 import { type NextPage } from 'next';
 import Sidebar from '../../components/sidebar';
+import { useForm, type SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+export const TeamLeaderPlusOfficeSchema = z.object({
+  name: z.string().min(1, 'Required'),
+  lastName: z.string().min(1, 'Required'),
+  email: z.string().email().min(1, 'Required'),
+  phoneNumber: z.string().min(1, 'Required'),
+  userName: z.string().min(1, 'Required'),
+  password: z.string().min(1, 'Required'),
+  officeName: z.string().optional(),
+  address: z.string().optional(),
+});
+
+type TeamLeaderPlusOfficeSchemaType = z.infer<typeof TeamLeaderPlusOfficeSchema>;
 
 const NewTeamLeader: NextPage = () => {
   const [formStep, setFormStep] = useState(0);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TeamLeaderPlusOfficeSchemaType>({
+    mode: 'onTouched',
+    resolver: zodResolver(TeamLeaderPlusOfficeSchema),
+  });
 
   const nextFormStep = () => {
     if (formStep == 1) {
@@ -11,6 +35,10 @@ const NewTeamLeader: NextPage = () => {
     } else {
       setFormStep(1);
     }
+  };
+
+  const handleForm: SubmitHandler<TeamLeaderPlusOfficeSchemaType> = (formValues) => {
+    console.log(formValues);
   };
 
   return (
@@ -22,7 +50,7 @@ const NewTeamLeader: NextPage = () => {
       />
       <div className='flex w-full justify-center'>
         <form
-          action=''
+          onSubmit={handleSubmit(handleForm)}
           className='relative flex max-w-xl flex-grow flex-col gap-2 rounded-md p-12 text-gray-300 sm:gap-8 sm:bg-cool-gray-700 sm:p-8'
         >
           <section className={`${formStep == 0 ? 'block' : 'hidden'} flex flex-col gap-2 sm:gap-8`}>
@@ -31,20 +59,24 @@ const NewTeamLeader: NextPage = () => {
             </div>
             <div className='flex flex-col gap-2 sm:flex-row sm:gap-6'>
               <div className='flex flex-grow flex-col'>
-                <label htmlFor='' className='pb-1 sm:pb-2'>
+                <label htmlFor='' className='flex items-center pb-1 sm:pb-2'>
                   Name
+                  <div className='pl-4 text-sm text-red-700'>{errors.name?.message}</div>
                 </label>
                 <input
+                  {...register('name')}
                   type='text'
                   placeholder='Name'
                   className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
                 />
               </div>
               <div className='flex flex-grow flex-col'>
-                <label htmlFor='' className='pb-1 sm:pb-2'>
+                <label htmlFor='' className='flex items-center pb-1 sm:pb-2'>
                   Last Name
+                  <div className='pl-4 text-sm text-red-700'>{errors.lastName?.message}</div>
                 </label>
                 <input
+                  {...register('lastName')}
                   type='text'
                   placeholder='Last name'
                   className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
@@ -52,20 +84,24 @@ const NewTeamLeader: NextPage = () => {
               </div>
             </div>
             <div className='flex flex-grow flex-col'>
-              <label htmlFor='' className='pb-1 sm:pb-2'>
+              <label htmlFor='' className='flex items-center pb-1 sm:pb-2'>
                 Email
+                <div className='pl-4 text-sm text-red-700'>{errors.email?.message}</div>
               </label>
               <input
+                {...register('email')}
                 type='email'
                 placeholder='Email'
                 className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
               />
             </div>
             <div className='flex flex-grow flex-col'>
-              <label htmlFor='' className='pb-1 sm:pb-2'>
+              <label htmlFor='' className='flex items-center pb-1 sm:pb-2'>
                 Phone Number
+                <div className='pl-4 text-sm text-red-700'>{errors.phoneNumber?.message}</div>
               </label>
               <input
+                {...register('phoneNumber')}
                 type='text'
                 placeholder='+54 000 0000000'
                 className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
@@ -73,20 +109,24 @@ const NewTeamLeader: NextPage = () => {
             </div>
             <div className='flex flex-col gap-2 sm:flex-row sm:gap-6'>
               <div className='flex flex-grow flex-col'>
-                <label htmlFor='' className='pb-1 sm:pb-2'>
+                <label htmlFor='' className='flex items-center pb-1 sm:pb-2'>
                   UserName
+                  <div className='pl-4 text-sm text-red-700'>{errors.userName?.message}</div>
                 </label>
                 <input
+                  {...register('userName')}
                   type='text'
                   placeholder='UserName'
                   className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
                 />
               </div>
               <div className='flex flex-grow flex-col'>
-                <label htmlFor='' className='pb-1 sm:pb-2'>
+                <label htmlFor='' className='flex items-center pb-1 sm:pb-2'>
                   Password
+                  <div className='pl-4 text-sm text-red-700'>{errors.password?.message}</div>
                 </label>
                 <input
+                  {...register('password')}
                   type='password'
                   placeholder='Password'
                   className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
@@ -100,20 +140,24 @@ const NewTeamLeader: NextPage = () => {
             </div>
             <div className='flex flex-col gap-2 sm:flex-row sm:gap-6'>
               <div className='flex flex-grow flex-col'>
-                <label htmlFor='' className='pb-1 sm:pb-2'>
-                  Name
+                <label htmlFor='' className='flex items-center pb-1 sm:pb-2'>
+                  Office's Name
+                  <div className='pl-4 text-sm text-red-700'>{errors.officeName?.message}</div>
                 </label>
                 <input
+                  {...register('officeName')}
                   type='text'
                   placeholder='Name of the Office'
                   className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
                 />
               </div>
               <div className='flex flex-grow flex-col'>
-                <label htmlFor='' className='pb-1 sm:pb-2'>
+                <label htmlFor='' className='flex items-center pb-1 sm:pb-2'>
                   Address
+                  <div className='pl-4 text-sm text-red-700'>{errors.address?.message}</div>
                 </label>
                 <input
+                  {...register('address')}
                   type='text'
                   placeholder='Inside/outside the building'
                   className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
