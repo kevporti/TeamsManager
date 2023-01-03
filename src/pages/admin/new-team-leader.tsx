f340c744e3b658de3bc230f681edac2d2700f9c7
Kevin <challengerenlol@gmail.com>
2023-01-03 18:23:51 -0300

    Styles finished for the two-step form to create User for TeamLeader and an Office. Also installed react hook form.

--- src/pages/admin/new-team-leader.tsx
+++ src/pages/admin/new-team-leader.tsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
 import { type NextPage } from 'next';
 import Sidebar from '../../components/sidebar';
 
 const NewTeamLeader: NextPage = () => {
+  const [formStep, setFormStep] = useState(0);
+
+  const nextFormStep = () => {
+    if (formStep == 1) {
+      setFormStep(0);
+    } else {
+      setFormStep(1);
+    }
+  };
+
   return (
     <div className='flex h-screen w-full items-center bg-cool-gray-900'>
       <Sidebar />
@@ -14,88 +25,130 @@ const NewTeamLeader: NextPage = () => {
           action=''
           className='relative flex max-w-xl flex-grow flex-col gap-2 rounded-md p-12 text-gray-300 sm:gap-8 sm:bg-cool-gray-700 sm:p-8'
         >
-          <div className='flex flex-col gap-2 sm:flex-row sm:gap-6'>
+          <section className={`${formStep == 0 ? 'block' : 'hidden'} flex flex-col gap-2 sm:gap-8`}>
+            <div>
+              <h1 className='text-xl sm:text-2xl'>Create a user for a Team Leader</h1>
+            </div>
+            <div className='flex flex-col gap-2 sm:flex-row sm:gap-6'>
+              <div className='flex flex-grow flex-col'>
+                <label htmlFor='' className='pb-1 sm:pb-2'>
+                  Name
+                </label>
+                <input
+                  type='text'
+                  placeholder='Name'
+                  className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
+                />
+              </div>
+              <div className='flex flex-grow flex-col'>
+                <label htmlFor='' className='pb-1 sm:pb-2'>
+                  Last Name
+                </label>
+                <input
+                  type='text'
+                  placeholder='Last name'
+                  className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
+                />
+              </div>
+            </div>
             <div className='flex flex-grow flex-col'>
               <label htmlFor='' className='pb-1 sm:pb-2'>
-                Name
+                Email
               </label>
               <input
-                required
+                type='email'
+                placeholder='Email'
+                className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
+              />
+            </div>
+            <div className='flex flex-grow flex-col'>
+              <label htmlFor='' className='pb-1 sm:pb-2'>
+                Phone Number
+              </label>
+              <input
                 type='text'
-                placeholder='Name'
+                placeholder='+54 000 0000000'
                 className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
               />
             </div>
-            <div className='flex flex-grow flex-col'>
-              <label htmlFor='' className='pb-1 sm:pb-2'>
-                Last Name
-              </label>
-              <input
-                required
-                type='text'
-                placeholder='Last name'
-                className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
-              />
+            <div className='flex flex-col gap-2 sm:flex-row sm:gap-6'>
+              <div className='flex flex-grow flex-col'>
+                <label htmlFor='' className='pb-1 sm:pb-2'>
+                  UserName
+                </label>
+                <input
+                  type='text'
+                  placeholder='UserName'
+                  className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
+                />
+              </div>
+              <div className='flex flex-grow flex-col'>
+                <label htmlFor='' className='pb-1 sm:pb-2'>
+                  Password
+                </label>
+                <input
+                  type='password'
+                  placeholder='Password'
+                  className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
+                />
+              </div>
             </div>
-          </div>
-          <div className='flex flex-grow flex-col'>
-            <label htmlFor='' className='pb-1 sm:pb-2'>
-              Email
-            </label>
-            <input
-              required
-              type='email'
-              placeholder='Email'
-              className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
-            />
-          </div>
-          <div className='flex flex-grow flex-col'>
-            <label htmlFor='' className='pb-1 sm:pb-2'>
-              Phone Number
-            </label>
-            <input
-              required
-              type='text'
-              placeholder='+54 000 0000000'
-              className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
-            />
-          </div>
-          <div className='flex flex-col gap-2 sm:flex-row sm:gap-6'>
-            <div className='flex flex-grow flex-col'>
-              <label htmlFor='' className='pb-1 sm:pb-2'>
-                UserName
-              </label>
-              <input
-                required
-                type='text'
-                placeholder='UserName'
-                className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
-              />
+          </section>
+          <section className={`${formStep == 1 ? 'block' : 'hidden'} flex flex-col gap-2 sm:gap-8`}>
+            <div>
+              <h1 className='text-xl sm:text-2xl'>Create an Office for the Team Leader</h1>
             </div>
-            <div className='flex flex-grow flex-col'>
-              <label htmlFor='' className='pb-1 sm:pb-2'>
-                Password
-              </label>
-              <input
-                required
-                type='password'
-                placeholder='Password'
-                className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
-              />
+            <div className='flex flex-col gap-2 sm:flex-row sm:gap-6'>
+              <div className='flex flex-grow flex-col'>
+                <label htmlFor='' className='pb-1 sm:pb-2'>
+                  Name
+                </label>
+                <input
+                  type='text'
+                  placeholder='Name of the Office'
+                  className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
+                />
+              </div>
+              <div className='flex flex-grow flex-col'>
+                <label htmlFor='' className='pb-1 sm:pb-2'>
+                  Address
+                </label>
+                <input
+                  type='text'
+                  placeholder='Inside/outside the building'
+                  className='flex rounded-md bg-cool-gray-700 p-1 sm:bg-cool-gray-900'
+                />
+              </div>
             </div>
-          </div>
-          <div className='flex flex-col-reverse gap-y-4 pt-8 sm:flex-row sm:justify-between sm:gap-y-0 sm:pt-0'>
+          </section>
+          <div
+            className={`${
+              formStep == 0 ? 'sm:flex-row' : 'sm:flex-row-reverse'
+            } flex flex-col-reverse gap-y-4 pt-8 sm:justify-between sm:gap-y-0 sm:pt-0`}
+          >
             <div>
               <button
-                className='w-full rounded-md border py-2 font-semibold text-gray-300 duration-300 hover:bg-gray-300 hover:py-3 hover:px-9 hover:text-gray-900 sm:w-auto sm:px-8'
+                className={`${
+                  formStep == 0
+                    ? 'border text-gray-300 hover:bg-gray-300 hover:text-gray-900'
+                    : 'border-none bg-[#FEA702] text-cool-gray-800 hover:bg-[#B77701]'
+                } w-full rounded-md py-2 px-8 font-semibold duration-300`}
                 type='submit'
               >
                 Submit
               </button>
             </div>
             <div>
-              <button className='w-full rounded-md bg-[#FEA702] py-2 font-semibold text-gray-900 duration-300 hover:bg-[#B77701] hover:py-3 hover:px-9 hover:font-bold sm:w-auto sm:px-8'>
-                Continue creating Office
+              <button
+                onClick={nextFormStep}
+                type='button'
+                className={`${
+                  formStep == 1
+                    ? 'border text-gray-300 hover:bg-gray-300 hover:text-gray-900'
+                    : 'border-none bg-[#FEA702] text-cool-gray-800 hover:bg-[#B77701]'
+                } w-full rounded-md py-2 px-8 font-semibold duration-300`}
+              >
+                {formStep == 0 ? 'Create Office' : 'Go Back'}
               </button>
             </div>
           </div>
