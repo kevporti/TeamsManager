38e59e528c4d0978419b16ea52194144bc7dd9cf
Lerskk <faustoleo02@gmail.com>
2023-01-06 12:58:53 -0300

    fix some issues with the custom component clear form when submitted fix #30

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,23 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 type Props = {
-  reference: string,
-  label: string,
-  error: any
-}
+  reference: string;
+  label: string;
+  error: string | undefined;
+};
 
-const Input = React.forwardRef<HTMLInputElement, Props>(({reference, label, error, ...rest}, ref) => {
+const Input = React.forwardRef<HTMLInputElement, Props>(({ reference, label, error, ...rest }, ref) => {
   return (
     <div className='flex flex-col gap-2'>
-      <label className='text-white' htmlFor={reference}>{label}</label>
-      <input className='h-8 rounded-md bg-cool-gray-900 p-1 text-white outline-none focus:border-b' name={reference} placeholder={label} {...rest} ref={ref}/>
-      {error ? (
-        <span className='font-bold text-red-500'>*{error.message}</span>
-      ) : (
-        <div className='h-6' />
-      )}
-    </ div>
+      <div className='flex gap-x-5'>
+        <label className='text-white' htmlFor={reference}>
+          {label}
+        </label>
+        {error && <span className='font-bold text-red-500'>{error}</span>}
+      </div>
+      <input
+        className='h-8 rounded-md bg-cool-gray-900 p-1 text-white outline-none focus:border-b'
+        name={reference}
+        placeholder={label}
+        {...rest}
+        ref={ref}
+      />
+    </div>
   );
-})
+});
 
-export default Input
+export default Input;
--- src/components/TicketForm.tsx
+++ src/components/TicketForm.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { useForm, type SubmitHandler} from 'react-hook-form';
+import { useForm, type SubmitHandler } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { trpc } from '@/utils/trpc';
 import { z } from 'zod';
-import Input from '@components/Input'
+import Input from '@components/Input';
 
 export const ticketSchema = z.object({
   title: z.string().min(1, 'Required'),
@@ -17,34 +17,52 @@ export const ticketSchema = z.object({
 
 type TicketSchemaType = z.infer<typeof ticketSchema>;
 
-
 export default function TicketForm() {
   const mutation = trpc.ticket.createTicket.useMutation();
 
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm<TicketSchemaType>({
     mode: 'onTouched',
     resolver: zodResolver(ticketSchema),
   });
 
-  const onSubmit: SubmitHandler<TicketSchemaType> = (data) => mutation.mutate(data);
+  const onSubmit: SubmitHandler<TicketSchemaType> = (data) => {
+    mutation.mutate(data);
+    reset({ title: '', nameOfSender: '', lastNameOfSender: '', emailOfSender: '', description: '' });
+  };
 
   return (
     <form className='flex flex-col rounded-md bg-cool-gray-700 p-8' onSubmit={handleSubmit(onSubmit)}>
-      <Input reference='title' label='Title' error={errors.title} {...register('title')} />
+      <Input reference='title' label='Title' error={errors.title?.message} {...register('title')} />
       <div className='p-2' />
       <div className='flex'>
-        <Input reference='nameOfSender' label='Name' error={errors.nameOfSender} {...register('nameOfSender')} />
+        <Input
+          reference='nameOfSender'
+          label='Name'
+          error={errors.nameOfSender?.message}
+          {...register('nameOfSender')}
+        />
         <div className='w-5' />
-        <Input reference='lastNameOfSender' label='Last name' error={errors.lastNameOfSender} {...register('lastNameOfSender')} />
+        <Input
+          reference='lastNameOfSender'
+          label='Last name'
+          error={errors.lastNameOfSender?.message}
+          {...register('lastNameOfSender')}
+        />
       </div>
       <div className='p-2' />
       <div className='flex w-full'>
         <div className='flex w-3/4 flex-col gap-2'>
-          <Input reference='emailOfSender' label='Email' error={errors.emailOfSender} {...register('emailOfSender')} />
+          <Input
+            reference='emailOfSender'
+            label='Email'
+            error={errors.emailOfSender?.message}
+            {...register('emailOfSender')}
+          />
         </div>
         <div className='w-5' />
         <div className='flex w-1/4 flex-col gap-2'>
@@ -63,17 +81,15 @@ export default function TicketForm() {
       </div>
       <div className='p-2' />
       <div className='flex flex-col gap-2'>
-        <label className='text-white'>Description</label>
+        <div className='flex gap-x-5'>
+          <label className='text-white'>Description</label>
+          {errors.description?.message && <span className='font-bold text-red-500'>{errors.description.message}</span>}
+        </div>
         <textarea
           className=' h-24 rounded-md bg-cool-gray-900 p-1 text-white outline-none focus:border-b '
           placeholder='Description'
           {...register('description')}
         />
-        {errors.description ? (
-          <span className='font-bold text-red-500'>*{errors.description.message}</span>
-        ) : (
-          <div className='h-6' />
-        )}
       </div>
       <button className='w-fit self-center rounded-md bg-light-sky-blue py-1 px-3 font-bold' type='submit'>
         Submit
